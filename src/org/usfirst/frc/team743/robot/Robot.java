853678bc1f07e585a262fc0a2bc860684d953660
Technobots <tchuinkam.germain.hsct@gmail.com>
2018-02-24 11:46:36 -0500

    Fix local variable reference to '_xboxController' issue

--- src/org/usfirst/frc/team743/robot/Robot.java
+++ src/org/usfirst/frc/team743/robot/Robot.java
@@ -137,8 +137,8 @@ public class Robot extends TimedRobot {
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-		mecanum.driveCartesian(m_oi.Controller.getX(Hand.kLeft)/255,
-		  m_oi.Controller.getY(Hand.kLeft)/255, m_oi.Controller.getX(Hand.kRight)/255);
+		mecanum.driveCartesian(m_oi._xboxController.getX(Hand.kLeft)/255,
+		  m_oi._xboxController.getY(Hand.kLeft)/255, m_oi._xboxController.getX(Hand.kRight)/255);
 	}
 
 	/**
